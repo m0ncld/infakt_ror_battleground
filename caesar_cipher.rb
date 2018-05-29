fda3389223d41646757dbc94e875c6a59fb12f8f
Łukasz Łuszczek <l.luszczek@visiona.pl>
2018-05-29 09:39:52 +0200

    Dodano rozwiazanie do testow

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,33 @@
-class CeasarCipher
+class CaesarCipher
+    @value
+    def initialize(text, rot = 13)
+        @value = text.chars.map(&:ord)
+        @value = @value.map do |chr|
+            if chr.between?(48, 57)
+                self.shift(chr, rot, 48, 57)
+            elsif chr.between?(65, 90)
+                self.shift(chr, rot, 65, 90)
+            elsif chr.between?(97, 122)
+                self.shift(chr, rot, 97, 122)
+            else
+                chr
+            end
+        end
+        @value = @value.map { |chr| chr.chr }.join
+    end
 
+    def perform
+        return @value
+    end
+
+    def shift (value, rot, min, max)
+        new_chr = value + rot
+        if new_chr.between?(min, max)
+            new_chr
+        elsif new_chr < min
+            max - (min - new_chr) + 1
+        else
+            (new_chr - max) + min - 1
+        end
+    end
 end
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,21 @@
 class PassedOrFailed
+    @result
+    def initialize(students, limit)
+        passed = Hash.new
+        failed = Hash.new
+        students.each do |name, mark|
+            if mark.to_i >= limit.to_i
+                passed[name] = mark.to_i
+            else
+                failed[name] = mark.to_i
+            end
+        end
+        @result = Hash.new
+        @result[:passed] = passed
+        @result[:failed] = failed
+    end
 
+    def perform
+        return @result
+    end
 end
